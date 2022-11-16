db6c8a2abd3ca6a888ff5977eb5cb078d4466349
Swaroop Khot <84902423+khotswaroop@users.noreply.github.com>
2022-11-16 08:40:29 +0530

    Add files via upload

---
+++ flatapp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from flatapp import views
+
+urlpatterns = [
+    path('',views.index),
+    path('register',views.register),
+    path('bookflat',views.bookflat),
+    path('showbooking',views.showflats),
+    path('delete/<rid>',views.deleteflats),
+    path('edit/<rid>',views.updateflats),
+    path('mumbai',views.mumbai),
+    path('bangalore',views.bangalore),
+    path('pune',views.pune),
+    path('delhi',views.delhi),
+    path('residency',views.residency),
+    path('registration',views.registration),
+    path('login',views.login),
+]
